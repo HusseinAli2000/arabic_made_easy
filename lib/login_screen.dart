f94888fca460c83190fe871a347de812de69a523
Hussein Ali <hussein.ali.2019@uni.strath.ac.uk>
2022-11-08 23:16:00 +0000

    Styled Login Screen UI

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -36,212 +36,246 @@ class _LoginScreenState extends State<LoginScreen> {
       home: Scaffold(
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 32, 6, 96),
+                  Color.fromARGB(255, 57, 119, 194),
+                ],
+              ),
+            ),
+          ),
           title: const Center(
             child: Text(
               'Arabic Made Easy',
               style: TextStyle(
                 fontFamily: 'Akaya',
                 fontSize: 30,
-                color: Colors.teal,
+                color: Color.fromARGB(255, 235, 234, 243),
                 fontWeight: FontWeight.bold,
               ),
             ),
           ),
-          backgroundColor: Colors.teal[100],
         ),
-        backgroundColor: Colors.teal[300],
-        body: Center(
-          child: SingleChildScrollView(
-            child: SafeArea(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const CircleAvatar(
-                    radius: 40.0,
-                    backgroundImage: AssetImage('images/icon.png'),
-                  ),
-                  const Text(
-                    'Welcome !',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 35,
-                      fontFamily: 'Caveat',
-                      color: Color.fromARGB(255, 205, 234, 232),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 125, 173, 232),
+                Color.fromARGB(255, 20, 0, 70),
+              ],
+            ),
+          ),
+          child: Center(
+            child: SingleChildScrollView(
+              child: SafeArea(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const CircleAvatar(
+                      backgroundColor: Color.fromARGB(255, 95, 123, 215),
+                      radius: 40.0,
+                      backgroundImage: AssetImage('images/icon4.png'),
                     ),
-                  ),
-                  const Text(
-                    'Sign In to acess your Arabic learning',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                      fontFamily: 'Caveat',
-                      color: Color.fromARGB(255, 205, 234, 232),
+                    const Text(
+                      'Welcome !',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35,
+                        fontFamily: 'Caveat',
+                        color: Color.fromARGB(255, 235, 234, 243),
+                      ),
                     ),
-                  ),
+                    const Text(
+                      'Sign In to acess your Arabic learning',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        fontFamily: 'Caveat',
+                        color: Color.fromARGB(255, 235, 234, 243),
+                      ),
+                    ),
 
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.teal[100],
-                        border: Border.all(color: Colors.white70),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 15.0),
-                        child: TextField(
-                          controller: emailController,
-                          decoration: const InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Email',
-                          ),
-                        ),
-                      ),
+                    const SizedBox(
+                      height: 15,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.teal[100],
-                        border: Border.all(color: Colors.white70),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 15.0),
-                        child: TextField(
-                          controller: passwordController,
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Password',
-                          ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 235, 234, 243),
+                          border: Border.all(color: Colors.white70),
+                          borderRadius: BorderRadius.circular(12),
                         ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    const ForgotPasswordPage(),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 15.0),
+                          child: TextField(
+                            controller: emailController,
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Email',
+                              hintStyle: TextStyle(
+                                color: Color.fromARGB(150, 20, 0, 75),
                               ),
-                            );
-                          },
-                          child: const Text(
-                            'Forgot Password?',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 235, 234, 243),
+                          border: Border.all(color: Colors.white70),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 15.0),
+                          child: TextField(
+                            controller: passwordController,
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Password',
+                              hintStyle: TextStyle(
+                                color: Color.fromARGB(150, 20, 0, 75),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      const ForgotPasswordPage(),
+                                ),
+                              );
+                            },
+                            child: const Text(
+                              'Forgot Password?',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 235, 234, 243),
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Akaya',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    // Padding(
+                    //   padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    //   child: GestureDetector(
+                    //     onTap: signIn,
+                    //     child: Container(
+                    //       padding: const EdgeInsets.all(20),
+                    //       decoration: BoxDecoration(
+                    //         color: Colors.teal,
+                    //         borderRadius: BorderRadius.circular(12),
+                    //       ),
+                    //       child: const Center(
+                    //         child: Text(
+                    //           'Sign In',
+                    //           style: TextStyle(
+                    //             color: Color.fromARGB(255, 205, 234, 232),
+                    //             fontWeight: FontWeight.bold,
+                    //             fontSize: 18,
+                    //           ),
+                    //         ),
+                    //       ),
+                    //     ),
+                    //   ),
+                    // ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(
+                          signIn,
+                        );
+                      },
+                      style: const ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(
+                          Color.fromARGB(130, 79, 64, 179),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: const [
+                          SizedBox(
+                            width: 30,
+                            height: 50,
+                          ),
+                          Text(
+                            'Sign In',
                             style: TextStyle(
-                              color: Color.fromARGB(255, 7, 82, 75),
+                              color: Color.fromARGB(255, 235, 234, 243),
+                              fontFamily: 'Caveat',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25.0,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 35,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Not a member? ',
+                          style: TextStyle(
+                            color: Color.fromARGB(100, 235, 234, 243),
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Akaya',
+                            fontSize: 16,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: widget.showRegisterPage,
+                          child: const Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 235, 234, 243),
                               fontWeight: FontWeight.bold,
                               fontFamily: 'Akaya',
+                              fontSize: 15,
                             ),
                           ),
                         ),
                       ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  // Padding(
-                  //   padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  //   child: GestureDetector(
-                  //     onTap: signIn,
-                  //     child: Container(
-                  //       padding: const EdgeInsets.all(20),
-                  //       decoration: BoxDecoration(
-                  //         color: Colors.teal,
-                  //         borderRadius: BorderRadius.circular(12),
-                  //       ),
-                  //       child: const Center(
-                  //         child: Text(
-                  //           'Sign In',
-                  //           style: TextStyle(
-                  //             color: Color.fromARGB(255, 205, 234, 232),
-                  //             fontWeight: FontWeight.bold,
-                  //             fontSize: 18,
-                  //           ),
-                  //         ),
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(
-                        signIn,
-                      );
-                    },
-                    style: const ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll(
-                          Color.fromARGB(255, 210, 235, 233)),
-                    ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: const [
-                        Text(
-                          'Sign In',
-                          style: TextStyle(
-                            color: Colors.teal,
-                            fontFamily: 'Caveat',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25.0,
-                          ),
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Not a member? ',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 205, 234, 232),
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Akaya',
-                          fontSize: 16,
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: widget.showRegisterPage,
-                        child: const Text(
-                          'Sign Up',
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 7, 82, 75),
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Akaya',
-                            fontSize: 15,
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
