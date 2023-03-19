ef499bdca8bc752038ffd7a8092866d8539b3521
Hussein Ali <hussein.ali.2019@uni.strath.ac.uk>
2023-03-19 02:31:07 +0000

    added multiple choice however not complete

--- lib/animals_multi_choice.dart
+++ lib/animals_multi_choice.dart
@@ -1,127 +1,87 @@
-import 'package:arabic_made_easy/second_page.dart';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
-
 import 'animals.dart';
+import 'tts_button_two.dart';
+import 'words.dart';
 
 class AnimalsMulti extends StatefulWidget {
-  const AnimalsMulti({super.key});
+  const AnimalsMulti({Key? key}) : super(key: key);
 
   @override
   State<AnimalsMulti> createState() => _AnimalsMultiState();
 }
 
 class _AnimalsMultiState extends State<AnimalsMulti> {
+  final List<String> _words = AnimalWords.toList();
+  late String _word;
+  late List<String> _options;
+  final _random = Random();
+
+  @override
+  void initState() {
+    super.initState();
+    _generateQuestion();
+  }
+
+  void _generateQuestion() {
+    _word = _words[_random.nextInt(_words.length)];
+    _options = [_word];
+    while (_options.length < 4) {
+      String randomWord = _words[_random.nextInt(_words.length)];
+      if (randomWord != _word && !_options.contains(randomWord)) {
+        _options.add(randomWord);
+      }
+    }
+    _options.shuffle();
+  }
+
+  void _onOptionSelected(String option) {
+    // Check if the selected option is correct
+    if (option == _word) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Correct!'),
+          content: Text('You got it right.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                _generateQuestion();
+                setState(() {});
+              },
+              child: Text('Next question'),
+            ),
+          ],
+        ),
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Incorrect'),
+          content: Text('Sorry, that was not the correct answer.'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('Try again'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        bottomNavigationBar: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color.fromARGB(255, 32, 6, 96),
-                Color.fromARGB(255, 57, 119, 194),
-              ],
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 9, vertical: 9),
-            child: GNav(
-              gap: 15,
-              padding: const EdgeInsets.all(6),
-              backgroundColor: Colors.transparent,
-              textStyle: const TextStyle(
-                fontFamily: 'Akaya',
-                fontSize: 18,
-                color: Color.fromARGB(255, 235, 234, 243),
-                fontWeight: FontWeight.bold,
-              ),
-              tabBackgroundColor: const Color.fromARGB(161, 6, 12, 58),
-              color: const Color.fromARGB(255, 235, 234, 243),
-              activeColor: const Color.fromARGB(255, 235, 234, 243),
-              onTabChange: (index) {
-                if (index == 0) {
-                  Future.delayed(
-                    const Duration(seconds: 1),
-                    () {
-                      setState(
-                        () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const Animals(),
-                            ),
-                          );
-                        },
-                      );
-                    },
-                  );
-                } else if (index == 1) {
-                  Future.delayed(
-                    const Duration(seconds: 1),
-                    () {
-                      setState(
-                        () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const PageTwo(),
-                            ),
-                          );
-                        },
-                      );
-                    },
-                  );
-                } else if (index == 2) {
-                  setState(() {});
-                }
-              },
-              tabs: const [
-                GButton(
-                  icon: Icons.arrow_back,
-                  text: 'Back',
-                ),
-                GButton(
-                  icon: Icons.class_,
-                  text: 'Classes',
-                ),
-                GButton(
-                  icon: Icons.settings,
-                  text: 'Settings',
-                ),
-              ],
-            ),
-          ),
-        ),
+        // Navigation bar code omitted for brevity
         appBar: AppBar(
-          flexibleSpace: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color.fromARGB(255, 32, 6, 96),
-                  Color.fromARGB(255, 57, 119, 194),
-                ],
-              ),
+            // AppBar code omitted for brevity
             ),
-          ),
-          title: const Center(
-            child: Text(
-              'Animals Quiz',
-              style: TextStyle(
-                fontFamily: 'Akaya',
-                fontSize: 25,
-                color: Color.fromARGB(255, 235, 234, 243),
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ),
         body: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
@@ -135,7 +95,25 @@ class _AnimalsMultiState extends State<AnimalsMulti> {
           ),
           child: Center(
             child: SafeArea(
-              child: Container(),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TTSButtonTwo(word: _word),
+                        SizedBox(height: 32),
+                        // Generate buttons for each option
+                        for (String option in _options)
+                          ElevatedButton(
+                            onPressed: () => _onOptionSelected(option),
+                            child: Text(option),
+                          ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
