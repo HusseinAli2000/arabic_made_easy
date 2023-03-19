d70de6d8a62b34c662ef8f80d65de3baef78e545
Hussein Ali <hussein.ali.2019@uni.strath.ac.uk>
2023-03-19 22:19:57 +0000

    animals MCQ done

--- lib/animals_multi_choice.dart
+++ lib/animals_multi_choice.dart
@@ -93,30 +93,87 @@ class _AnimalsMultiState extends State<AnimalsMulti> {
       barrierDismissible: false,
       context: context,
       builder: (context) => AlertDialog(
-        title: Text('Game over!'),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('You got $_score out of $_questionIndex correct.'),
-            SizedBox(height: 16),
-            Text('Percentage correct: ${percentage.toStringAsFixed(2)}%'),
-          ],
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              _progress = 0;
-              Navigator.pop(context);
-              _score = 0;
-              _questionIndex = 0;
-              _gameOver = false;
-              _words.shuffle(); // Shuffle the list of words.
-              _generateQuestion();
-              setState(() {});
-            },
-            child: Text('New game'),
+        contentPadding: const EdgeInsets.all(0),
+        insetPadding: const EdgeInsets.fromLTRB(90, 170, 90, 190),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        content: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: const LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 32, 6, 96),
+                Color.fromARGB(255, 57, 119, 194),
+              ],
+            ),
           ),
-        ],
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: Text(
+                  'You got $_score out of $_questionIndex correct.',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontFamily: 'Akaya',
+                    fontSize: 18,
+                    color: Color.fromARGB(255, 235, 234, 243),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: Text(
+                  'Percentage correct: ${percentage.toStringAsFixed(2)}%',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontFamily: 'Akaya',
+                    fontSize: 18,
+                    color: Color.fromARGB(255, 235, 234, 243),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 7),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.only(left: 50, right: 50),
+                        backgroundColor: const Color.fromARGB(130, 35, 61, 155),
+                        textStyle: const TextStyle(
+                          color: Color.fromARGB(255, 235, 234, 243),
+                          fontFamily: 'Akaya',
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      onPressed: () {
+                        _progress = 0;
+                        Navigator.pop(context);
+                        _score = 0;
+                        _questionIndex = 0;
+                        _gameOver = false;
+                        _words.shuffle(); // Shuffle the list of words.
+                        _generateQuestion();
+                        setState(() {});
+                      },
+                      child: Text('New Quiz'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -128,9 +185,90 @@ class _AnimalsMultiState extends State<AnimalsMulti> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          bottomNavigationBar: Container(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        bottomNavigationBar: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 32, 6, 96),
+                Color.fromARGB(255, 57, 119, 194),
+              ],
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 9, vertical: 9),
+            child: GNav(
+              gap: 15,
+              padding: const EdgeInsets.all(6),
+              backgroundColor: Colors.transparent,
+              textStyle: const TextStyle(
+                fontFamily: 'Akaya',
+                fontSize: 18,
+                color: Color.fromARGB(255, 235, 234, 243),
+                fontWeight: FontWeight.bold,
+              ),
+              tabBackgroundColor: const Color.fromARGB(161, 6, 12, 58),
+              color: const Color.fromARGB(255, 235, 234, 243),
+              activeColor: const Color.fromARGB(255, 235, 234, 243),
+              onTabChange: (index) {
+                if (index == 0) {
+                  Future.delayed(
+                    const Duration(seconds: 1),
+                    () {
+                      setState(
+                        () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Animals(),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  );
+                } else if (index == 1) {
+                  Future.delayed(
+                    const Duration(seconds: 1),
+                    () {
+                      setState(
+                        () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const PageTwo(),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  );
+                } else if (index == 2) {
+                  setState(() {});
+                }
+              },
+              tabs: const [
+                GButton(
+                  icon: Icons.arrow_back,
+                  text: 'Back',
+                ),
+                GButton(
+                  icon: Icons.class_,
+                  text: 'Classes',
+                ),
+                GButton(
+                  icon: Icons.settings,
+                  text: 'Settings',
+                ),
+              ],
+            ),
+          ),
+        ),
+        appBar: AppBar(
+          flexibleSpace: Container(
             decoration: const BoxDecoration(
               gradient: LinearGradient(
                 begin: Alignment.topLeft,
@@ -141,175 +279,95 @@ class _AnimalsMultiState extends State<AnimalsMulti> {
                 ],
               ),
             ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 9, vertical: 9),
-              child: GNav(
-                gap: 15,
-                padding: const EdgeInsets.all(6),
-                backgroundColor: Colors.transparent,
-                textStyle: const TextStyle(
-                  fontFamily: 'Akaya',
-                  fontSize: 18,
-                  color: Color.fromARGB(255, 235, 234, 243),
-                  fontWeight: FontWeight.bold,
-                ),
-                tabBackgroundColor: const Color.fromARGB(161, 6, 12, 58),
-                color: const Color.fromARGB(255, 235, 234, 243),
-                activeColor: const Color.fromARGB(255, 235, 234, 243),
-                onTabChange: (index) {
-                  if (index == 0) {
-                    Future.delayed(
-                      const Duration(seconds: 1),
-                      () {
-                        setState(
-                          () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const Animals(),
-                              ),
-                            );
-                          },
-                        );
-                      },
-                    );
-                  } else if (index == 1) {
-                    Future.delayed(
-                      const Duration(seconds: 1),
-                      () {
-                        setState(
-                          () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const PageTwo(),
-                              ),
-                            );
-                          },
-                        );
-                      },
-                    );
-                  } else if (index == 2) {
-                    setState(() {});
-                  }
-                },
-                tabs: const [
-                  GButton(
-                    icon: Icons.arrow_back,
-                    text: 'Back',
-                  ),
-                  GButton(
-                    icon: Icons.class_,
-                    text: 'Classes',
-                  ),
-                  GButton(
-                    icon: Icons.settings,
-                    text: 'Settings',
-                  ),
-                ],
+          ),
+          title: const Center(
+            child: Text(
+              'Animals Multiple Choice Quiz',
+              style: TextStyle(
+                fontFamily: 'Akaya',
+                fontSize: 25,
+                color: Color.fromARGB(255, 235, 234, 243),
+                fontWeight: FontWeight.bold,
               ),
             ),
           ),
-          appBar: AppBar(
-            flexibleSpace: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Color.fromARGB(255, 32, 6, 96),
-                    Color.fromARGB(255, 57, 119, 194),
-                  ],
-                ),
-              ),
-            ),
-            title: const Center(
-              child: Text(
-                'Animals Multiple Choice Quiz',
-                style: TextStyle(
-                  fontFamily: 'Akaya',
-                  fontSize: 25,
-                  color: Color.fromARGB(255, 235, 234, 243),
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 125, 173, 232),
+                Color.fromARGB(255, 20, 0, 70),
+              ],
             ),
           ),
-          body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color.fromARGB(255, 125, 173, 232),
-                  Color.fromARGB(255, 20, 0, 70),
-                ],
-              ),
-            ),
-            child: Center(
-              child: SafeArea(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 50),
-                        child: Image.asset(
-                          'images/${_word.replaceAll(' ', '')}.png',
-                          fit: BoxFit.fill,
-                        ),
+          child: Center(
+            child: SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 50),
+                      child: Image.asset(
+                        'images/${_word.replaceAll(' ', '')}.png',
+                        fit: BoxFit.fill,
                       ),
                     ),
-                    Expanded(
-                      child: TTSButtonTwo(
-                        word: _word,
-                      ),
+                  ),
+                  Expanded(
+                    child: TTSButtonTwo(
+                      word: _word,
                     ),
-                    // Generate buttons for each option
-                    for (int i = 0; i < _options.length; i++)
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(15, 0, 15, 5),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            ElevatedButton(
-                              onPressed: () => _onOptionSelected(_options[i]),
-                              style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all(_optionsColor[i]),
-                                shape: MaterialStateProperty.all<
-                                    RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
+                  ),
+                  // Generate buttons for each option
+                  for (int i = 0; i < _options.length; i++)
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 0, 15, 5),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () => _onOptionSelected(_options[i]),
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(_optionsColor[i]),
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
                                 ),
                               ),
-                              child: Text(
-                                _options[i],
-                                style: const TextStyle(color: Colors.white),
-                              ),
                             ),
-                          ],
-                        ),
+                            child: Text(
+                              _options[i],
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
                       ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(15, 20, 15, 30),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: LinearProgressIndicator(
-                          value: _progress,
-                          minHeight: 20,
-                          color: const Color.fromARGB(255, 35, 61, 155),
-                          backgroundColor:
-                              const Color.fromARGB(255, 235, 234, 243),
-                        ),
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(15, 20, 15, 30),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: LinearProgressIndicator(
+                        value: _progress,
+                        minHeight: 20,
+                        color: const Color.fromARGB(255, 35, 61, 155),
+                        backgroundColor:
+                            const Color.fromARGB(255, 235, 234, 243),
                       ),
-                    )
-                  ],
-                ),
+                    ),
+                  )
+                ],
               ),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
